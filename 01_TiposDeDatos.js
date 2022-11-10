235554d421892fdbd32f0c9cab49ddc50d87f494
oropezarodrigo <54340899+oropezarodrigo@users.noreply.github.com>
2022-11-10 08:01:38 -0500

    Add files via upload

---
+++ 01_TiposDeDatos.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * Tipos de datos
+ *
+ */
+
+// numero  --- number
+console.log(27);
+console.log(27.56);
+
+// texto -- string
+
+console.log("Rodrigo Oropeza");
+console.log("Hola, soy rodri");
+console.log (`Hola, soy oropeza`);
+
+// Logicos -- booleans
+console.log(true);
+console.log(false);
+
+// indefinido -- undefined
+
+console.log(undefined);
+
+// null
+console.log(null);
+
+/* 
+
+Tipos de datos estructurales
+
+*/
+
+// Objeto -- object
+
+console.log({
+  nombre: "Rodrigo",
+  edad: 22,
+  fechaDeNacimiento: null,
+  esAlto: true,
+  Ciudades: ["Cancun", "Jinotega", "Roma"],
+  direccion: { callePrincipal: "Porto Bello" },
+});
+
+// tipo de dato de tipo lista -- arrays
+console.log(["Mexico", "Nicaragua", "Ecuador"]);
+console.log([21, true, { nombre: "Rodrigo" }, [1, 2, 3, 4, 5]]);
